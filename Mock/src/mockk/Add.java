6282c0096498c758d352feb1905dac73c8f81f0a
kaviyarasan <akavi4706@gmail.com>
2022-03-31 10:20:31 +0530

    adding done

---
+++ Mock/src/mockk/Add.java
@@ -0,0 +1,10 @@
+package mockk;
+
+public class Add {
+	public static void main(String[] args) {
+		String [] a= {"1 2 3 4 5"};
+		
+		System.out.println(a[0]+a[1]+a[2]+a[3]+a[4]);
+	}
+
+}
---
+++ Mock/src/mockk/Example.java
@@ -0,0 +1,15 @@
+package mockk;
+
+public class Example {
+	public static void main(String[] args) {
+		String a={"kavi","07","vellore","onesoft"};
+		String b=a.split(",");
+		
+
+		System.out.println(a[0]+","+a[1]+","+a[2]+","+a[3]);
+		
+		
+		
+	}
+
+}
